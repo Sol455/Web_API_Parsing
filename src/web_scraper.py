2fc97cd9c4a8d4c354f29d8284bd7fcbabdc46d7
Sol Harter <92319512+Sol455@users.noreply.github.com>
2023-03-08 16:23:33 +0000

    Implemented sorting and printing of genres

--- src/web_scraper.py
+++ src/web_scraper.py
@@ -6,9 +6,23 @@ def retrieve_packs():
     packs = requests.get('https://api.ampifymusic.com/packs')
     return packs.json()
 
+def get_sort_genres(json_data):
+    genre_names = set()
+
+    for pack in json_data["packs"]:
+        for genre in pack["genres"]:
+            genre_names.add(genre)
+    sort_genre_names = sorted(genre_names)
+    return sort_genre_names
+
 def main():
     json_data = retrieve_packs()
-    print(json_data)
+    genre_names = get_sort_genres(json_data)\
+    #print(json_data)
+    print("\n%s genres found in packs:\n" % len(genre_names))
+
+    for genre in genre_names:
+        print("%s" % genre)
 
 if __name__ == "__main__":
     main()
